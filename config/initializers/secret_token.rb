9d7e754d855d28bb2f88acd59e7eb1188bd9f78a
Anton Redozubov <anton@redozubov.ru>
2012-05-18 14:17:43 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EagleLanding::Application.config.secret_token = 'e3635ab12f36c9663589fa2980daa1ed21b4a3083d701433517b86ecf765bdbc4ddab5996eed11408d4ff50f6c2b21ab5ec9859f6f0a4a9fd7e214ea794826bd'
